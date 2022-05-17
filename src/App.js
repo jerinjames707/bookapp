af1ec8106920c3cb25b1dc8d9c2364c78d2497b0
jerinjames707 <jerinjames707@gmail.com>
2022-05-17 12:01:46 +0530

    book app-addbook cmplt

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Addbook from './components/Addbook';
 
 function App() {
   return (
     <div>
-      
+      <Addbook/>
     </div>
   );
 }
