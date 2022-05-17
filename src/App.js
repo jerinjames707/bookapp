2b4a5555d55f89262406bef435ceaaaff84f37c7
jerinjames707 <jerinjames707@gmail.com>
2022-05-17 12:17:20 +0530

    book app-searchbook cmplt

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addbook from './components/Addbook';
+import Searchbook from './components/Searchbook';
 
 function App() {
   return (
     <div>
       <Addbook/>
+      <Searchbook/>
     </div>
   );
 }
